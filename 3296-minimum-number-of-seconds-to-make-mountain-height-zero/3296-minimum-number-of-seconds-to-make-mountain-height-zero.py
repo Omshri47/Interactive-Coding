b42d29b3ccbac69313e753cec6674e0ab6928a6a
Omshri Patel <omshripate47@gmail.com>
2026-03-13 23:51:27 +0530

    Time: 319 ms (100%), Space: 13.2 MB (100%) - LeetHub

---
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -0,0 +1,39 @@
+import math
+
+class Solution(object):
+    def minNumberOfSeconds(self, mountainHeight, workerTimes):
+        def can_reduce(total_time):
+            reduced_height = 0
+            for w in workerTimes:
+                # Corrected the parenthesis and math logic
+                # Solving: w * x * (x + 1) / 2 <= total_time
+               
+                x = (-1 + math.sqrt(1 + ((8 * total_time) // w))) // 2
+                reduced_height += x
+                if reduced_height >= mountainHeight:
+                    return True
+            return False
+
+        # Binary search bounds
+        low = 1
+        # The worst case is the fastest worker doing all the work
+        fastest_worker = min(workerTimes)
+        high = fastest_worker * mountainHeight * (mountainHeight + 1) // 2
+        
+        ans = high
+        while low <= high:
+            mid = (low + high) // 2
+            if can_reduce(mid):
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+                
+        """
+        solve w * x(x+1)/2 <= total_time
+        :type mountainHeight: int
+        :type workerTimes: List[int]
+        :rtype: int
+        """
+        
