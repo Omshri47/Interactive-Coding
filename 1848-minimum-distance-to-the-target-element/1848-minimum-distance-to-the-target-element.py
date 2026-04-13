062cd9f43284b6f8a8ece53559e4fa50fc3746a3
Omshri Patel <omshripate47@gmail.com>
2026-04-14 00:21:43 +0530

    Time: 0 ms (100%), Space: 12.5 MB (29.33%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def getMinDistance(self, nums, target, start):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        min_dist = float('inf')
+        
+        for i, num in enumerate(nums):
+            if num == target:
+                # Calculate absolute distance
+                current_dist = abs(i - start)
+                
+                # Update the minimum distance found so far
+                if current_dist < min_dist:
+                    min_dist = current_dist
+                    
+                # Optimization: if distance is 0, we can't do better
+                if min_dist == 0:
+                    return 0
+                    
+        return int(min_dist)
